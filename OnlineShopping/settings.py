e5c08ab348b5c5c5e2331317c05a27ede39fbe19
Kelvin Kelechi Ibeh <81246393+papikelscode@users.noreply.github.com>
2023-03-04 21:40:28 +0100

    Update settings.py

--- OnlineShopping/settings.py
+++ OnlineShopping/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'ujsx7y@d*q^b)ta6v6=v!3)ut4e0-zsg6xtlfp3qirfp5$#(3q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
